9b69f1947e9ea3c952a095fccdd5abdafdc6ed87
shannonperrins-lang <shannon.perrin@ucas-edu.net>
2026-09-10 14:32:46 -0600

    Variables practice

---
+++ Variables_practice.py
@@ -0,0 +1,13 @@
+# SP, Variables practice
+
+name= "shannon"
+number = "8"
+bignumber = "50"
+breakfeast = "nothing"
+color = "blue"
+school = "UCAS"
+eye = "gray"
+age = "13"
+subject = "math"
+year = "2026"
+print (name,number,bignumber,breakfeast,color,school,eye,age,subject,year)
