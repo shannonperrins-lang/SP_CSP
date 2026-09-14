0eddf1d92a7362d110e3852a6cab3a8c04ca3a16
shannonperrins-lang <shannon.perrin@ucas-edu.net>
2026-09-14 14:50:07 -0600

    logical and phyical memory notes

--- logical_memory.py
+++ logical_memory.py
@@ -1,9 +1,37 @@
 # SP, Logical and physical memory notes
 
-pi = 3.141592
+name = "Alex"
 
-school = "UCAS"
+age = 14
 
-print(f"the vriable pi has a value of {pi} and is saved at {id(pi)}")
+score = 10
 
-print(f"the variable school has a value of {school} and is saved at {id(school)}") 
+score =25
+
+height = 5.9
+
+city = "Denver"
+
+zip_code = 80202
+
+price1 = 10
+
+price2 = 10
+
+print(f"the vriable name has a value of {name} and is saved at {id(age)}")
+
+print(f"the vriable age has a value of {age} and is saved at {id(age)}")
+
+print(f"the vriable score value of {score} and is saved at {id(score)}")
+
+print(f"the vriable as score value of {score} and is saved at {id(score)}")
+
+print(f"the vriable as height value of {height} and is saved at {id(height)}")
+
+print(f"the vriable as city value of {city} and is saved at {id(city)}")
+
+print(f"the vriable as zip_code value of { zip_code} and is saved at {id(zip_code)}")
+
+print(f"the vriable as price1 value of {price1} and is saved at {id(price1)}")
+
+print(f"the vriable as price2  value of {price2} and is saved at {id(price2)}")
