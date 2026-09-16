e5bf549cf63de3d2ea61152bdd4566839537aa2b
shannonperrins-lang <shannon.perrin@ucas-edu.net>
2026-09-16 14:53:13 -0600

    intergers

---
+++ expressions.py
@@ -0,0 +1,35 @@
+# SP 7th intergers,flosts, and experrsions notes
+
+#integer => is a whole number 
+
+people = 24
+cars = 50 
+computers = 29
+awareness = -12
+
+# float => number with decimals
+
+pi = 3.1415
+temp = -95.6
+cost = 1.99
+rain = 2.17
+
+#arithmetic opertaors (+,-,*,/,//,**,%)
+
+print(f"18/4 is {18/4} the remander is {18%4}")
+print(f"{18/5} or {18/5} the remainder is {18%5}")
+
+# order of operations 
+grades = [85,66,94,72]
+students = len(grades)
+average = sum(grades)/students
+
+print(f"the average is {int (average)}")
+
+# CONVERT THE DATA TYPE
+
+price = float(input("how much did the item cost")
+tax = 0.0485
+sale_tax = price * tax
+total = price + sale_tax
+print(f"Your total is {total}")
