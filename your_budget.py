6224f9ed676986649d5f94deda1e3416d80c833f
shannonperrins-lang <shannon.perrin@ucas-edu.net>
2026-09-17 14:17:46 -0600

    nothing

---
+++ your_budget.py
@@ -0,0 +1,9 @@
+#SP your budget
+
+while True:
+        Try:
+        Income = float(input ("what is your monthly income?:"))
+        break
+Except:
+print("please enter a valud number for your income")
+
