ebac1119c1558aafa45f0073cc7b34f2f6feb674
shannonperrins-lang <shannon.perrin@ucas-edu.net>
2026-09-21 13:56:15 -0600

    your_budget

--- your_budget.py
+++ your_budget.py
@@ -43,4 +43,7 @@ print(f"your groceries is ${groceries:.2f} and that is {int(groceries/Income*100
 
 print(f"your transprotions is ${transprotions:.2f} and that is {int(transprotions/Income*100)}")
 
-print(f"you should save ")
+saving = Income * 0.10
+print(f"you should save ${saving} and thats 10% of your income")
+spending_money = Income - rent - utilities - groceries - transportation - saving 
+print(f"${spending_money} is your spening money")
