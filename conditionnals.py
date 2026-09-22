32a35389aef92c8b8a17476b2171fac546111bfb
shannonperrins-lang <shannon.perrin@ucas-edu.net>
2026-09-22 14:53:34 -0600

    conditionals

---
+++ conditionnals.py
@@ -0,0 +1,19 @@
+#SP, conditional notes
+
+#conditonanal
+
+time = 1416
+day = "tuesday"
+
+if time < 1200 and time >500:
+    print("good morning!")
+elif time < 1700:
+    print("good afternoon!")
+    if day != "saturday" and day !="sunday"
+        print("how has school been?")
+elif time < 2000:
+    print("good eveing")
+else:
+    print("good night!")
+
+print("code is done")
